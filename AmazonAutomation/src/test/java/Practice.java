717316e2a8965c64cfaaeeda0f7e9e859e1d6c5e
abhishek.ag.gupta <abhishek.ag.gupta@accenture.com>
2018-01-11 15:52:01 +0530

    First commit

---
+++ AmazonAutomation/src/test/java/Practice.java
@@ -0,0 +1,19 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fn();
+	}
+public static  int fn()
+{
+	int i;
+	int result = 0;
+
+	for(i = 0; i < 8; i++)
+	{
+	result = result + 7;
+	}
+	System.out.println(result);
+	return result;}
+}
