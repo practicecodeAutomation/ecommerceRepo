f6f18fdbfb34378f9fd7f02d192c18e1604ed2e5
practicecodeautomation <practicecodeautomation@gmail.com>
2018-01-11 16:22:25 +0530

    WorkItem:Dev test commit

---
+++ AmazonAutomation/src/test/java/Practice2.java
@@ -0,0 +1,15 @@
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fn();
+	}
+public static  void fn()
+{
+	int a=4;
+	int b=8;
+	System.out.println("Demo test commit"+a+b);
+	
+}
+}
